1220461b3b1dda7f2689b44a9e5d4b055cfbaea8
mzwwy <wahyucheger51@gmail.com>
2022-11-22 22:32:59 +0700

    Upload New File

---
+++ Test Suites/Penjumlahan.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Penjumlahan</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>e7c55c17-5875-40bf-b97a-746055f71729</testSuiteGuid>
+   <testCaseLink>
+      <guid>32ce2f4a-1825-4289-a58f-8b24aabf1039</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Penjumlahan</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
